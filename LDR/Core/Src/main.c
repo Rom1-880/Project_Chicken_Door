143a6260a37f585782dec43dd4f2af2f313e7420
install <install@D122-21.0810004P.local>
2026-03-26 16:26:09 +0100

    Changement de la pin PA0 car besoin pour la mesure d'energie -------------------- - Changement de la PIN PA0 à PA1 car besoin de PA0 pour la mesure d'energie - Etalonnage de la RFix - Liaison série affiche lux - Volt ok --------------------
    A revoir peu être l'étalonage

--- LDR/Core/Src/main.c
+++ LDR/Core/Src/main.c
@@ -265,7 +265,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLINGTIME_COMMON_1;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
