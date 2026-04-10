38d7e33072b909e7f2a26469efe59b5fb8bf4df2
tallam <tallam@D122-24.0810004P.local>
2026-04-10 10:25:12 +0200

    Additions des Bibliotheques
    Dans Core/Inc :
    affichagemenu.h
    lcd.h
    graphics.h
    fonts.h
    color.h
    
    Dans Core/Src :
    lcd.c
    graphics.c
    affichagemenu.c
    
    Manque bibliotheque qui exploitentent WriteData

---
+++ Tarek-carte_sans_ecran_V1.0/Core/Inc/affichagemenu.h
@@ -0,0 +1,57 @@
+/*
+ * affichagemenu.h
+ *
+ *  Created on: 8 mai 2019
+ *      Author: Joel
+ */
+
+#ifndef AFFICHAGEMENU_H_
+#define AFFICHAGEMENU_H_
+
+void affichecarre(char choix);      // encadrement rouge
+
+void effaceligne(void);              // efface la ligne correspondant à l'année.
+
+void effaceligne2(void);            // efface la ligne correspondant à la latitude
+
+void effaceligne3(void);            // efface la ligne correspondant à la longitude
+
+void afficheecran1(void);   // langue
+
+void afficheecran2(void);   // réglages
+
+void afficheecran3(void);   // réglage année
+
+void afficheecran4(void);   // réglage mois
+
+void afficheecran5(void);   // réglage jour
+
+void afficheecran6(void);   // réglage heure
+
+void afficheecran7(void);   // réglage min
+
+void afficheecran8(void);   // Ecran principal (après un appui de 5s sur le BP ON)
+
+void afficheecran9(void);   // réglage GPS
+
+void afficheecran10(void);  // choix mode ouverture
+
+void afficheecran11(void);  // Reglage heure mode ouverture fixe
+
+void afficheecran12(void);  // Reglage min mode ouverture fixe
+
+void afficheecran13(void);  // choix sensibilité capteur J/N Ouverture
+
+void afficheecran20(void);  // choix mode fermeture
+
+void afficheecran21(void);  // Reglage heure mode fermeture fixe
+
+void afficheecran22(void);  // Reglage min mode fermeture fixe
+
+void afficheecran23(void);  // choix sensibilité capteur J/N Fermeture
+
+void afficheecran24(void);  // réglage delai fermeture
+
+
+
+#endif /* AFFICHAGEMENU_H_ */
