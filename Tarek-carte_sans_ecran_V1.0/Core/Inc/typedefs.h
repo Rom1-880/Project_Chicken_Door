67bb32c2a45e209bbd27d68a0e406e4e57f64084
tallam <tallam@D122-24.0810004P.local>
2026-04-17 15:18:13 +0200

    🏆 Le Grand Bilan (Ce que tu as accompli) Ton projet repose maintenant sur 4 piliers très solides :
    L'Architecture Matérielle (Le bon projet) :
    
    Tu as identifié un bug profond (le conflit entre le U031 et ton U083) et
    tu as proprement "déménagé" ton code dans un projet sain.
    
    Tu as correctement configuré le bus SPI en 8 bits (la clé pour que
    l'écran te comprenne) et réglé la vitesse avec le Prescaler.
    
    Le Pilote de l'Écran (La couche basse : lcd.c / lcd.h) :
    
    Tu as écrit un "pont" parfait entre le STM32 et la puce ST7735 en
    utilisant les fonctions HAL (HAL_SPI_Transmit, HAL_GPIO_WritePin).
    
    Tu gères maintenant physiquement le composant : le Reset, la commande
    (A0), la sélection (CS), et l'alimentation de la dalle (ALIM_AFF).
    
    La Bibliothèque Graphique (La couche haute : graphics.c / fonts.h) :
    
    Tu as nettoyé l'ancien code pour le rendre universel (standard C moderne
    avec <stdint.h>).
    
    Ton STM32 est maintenant capable non seulement d'allumer des pixels,
    mais aussi de tracer des lignes, des cercles, et surtout d'écrire du
    texte avec différentes polices !
    
    L'Intelligence du Système (main.c) :
    
    Communication UART : Tu as créé un système sous interruption capable de
    lire des mots entiers ("MENU", "UP", "DOWN") envoyés par le PC sans
    bloquer le programme.
    
    Gestion d'énergie (Veille) : Tu as développé un chronomètre intelligent
    (HAL_GetTick()) qui compte 10 secondes d'inactivité pour éteindre
    l'écran de lui-même.
    
    📊 Bilan à jour et Sous-tâches
    Tâche du Cahier des Charges			Progression			Ce qui a été accompli
    1. Affichage des écrans TFT			50 %				L'écran s'allume et fonctionne !
    Reste à dessiner les menus finaux.
    2. Insertion des données (TFT)		20 %				La bibliothèque graphique
    fonctionne.
    3. Passage d'un écran via série		📉 50 %				RÉGRESSION : La
    communication a fonctionné mais ne répond plus. À déboguer en priorité.
    4. Mise en veille (10s)				80 %				Le chrono fonctionne. En attente de
    la série pour tester le réveil.
    5. Touches capacitives réelles		0 %					Toujours mis de côté en
    attendant l'équipe.
    6. Éphéméride (Heure + GPS)			0 %					Prévu pour plus tard.

---
+++ Tarek-carte_sans_ecran_V1.0/Core/Inc/typedefs.h
@@ -0,0 +1,19 @@
+/*
+ * typedefs.h
+ *
+ *  Created on: Nov 2, 2012
+ *      Author: RobG
+ */
+
+#ifndef TYPEDEFS_H_
+#define TYPEDEFS_H_
+
+#ifndef U_TYPES
+#define U_TYPES
+typedef unsigned char u_char;
+typedef unsigned int u_int;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+#endif
+
+#endif /* TYPEDEFS_H_ */
