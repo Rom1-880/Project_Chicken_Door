fd9f8c5b890a3fc7040624e405001d651f25dcf2
tallam <tallam@D122-24.0810004P.local>
2026-05-05 11:35:03 +0200

    Initialisation et fonctionnement valide l'écran marche les touches up down et menu marchent "on" marche mais probleme avec le changement de langue français est choisi toulout le temps

---
+++ Tarek-carte_sans_ecran_V1.0/Debug/Core/Src/affichagemenu.d
@@ -0,0 +1,8 @@
+Core/Src/affichagemenu.o: ../Core/Src/affichagemenu.c ../Core/Inc/color.h \
+ ../Core/Inc/graphics.h ../Core/Inc/typedefs.h ../Core/Inc/lcd.h \
+ ../Core/Inc/config.h
+../Core/Inc/color.h:
+../Core/Inc/graphics.h:
+../Core/Inc/typedefs.h:
+../Core/Inc/lcd.h:
+../Core/Inc/config.h:
---
+++ Tarek-carte_sans_ecran_V1.0/Debug/Core/Src/graphics.d
@@ -0,0 +1,6 @@
+Core/Src/graphics.o: ../Core/Src/graphics.c ../Core/Inc/graphics.h \
+ ../Core/Inc/typedefs.h ../Core/Inc/lcd.h ../Core/Inc/config.h
+../Core/Inc/graphics.h:
+../Core/Inc/typedefs.h:
+../Core/Inc/lcd.h:
+../Core/Inc/config.h:
