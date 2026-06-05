b13fbfd6c26738d9a1ea43f10839adec15fad050
install <install@D122-21.0810004P.local>
2026-06-05 13:28:39 +0200

    LDR Final Code comme ce qu'il y a dans LDR mais séparer les fonctions du main.c (dans power_management.c et power_management.h)

---
+++ LDR_Final/Core/Inc/power_management.h
@@ -0,0 +1,22 @@
+#ifndef POWER_MANAGEMENT_H
+#define POWER_MANAGEMENT_H
+
+#include "main.h"
+
+
+/* Handles externes (définis dans main.c) ------------------------------------*/
+extern ADC_HandleTypeDef  hadc1;
+extern UART_HandleTypeDef huart2;
+extern RTC_HandleTypeDef  hrtc;
+
+/* Variables externes (définies dans main.c) ---------------------------------*/
+extern uint32_t         adc_buffer[2];
+extern volatile uint8_t adc_ready;
+
+/* Define batterie  ---------------------------------*/
+#define OFFSET_BAT  0.112f
+
+/* Prototypes ----------------------------------------------------------------*/
+void Aller_Au_Dodo(void);
+
+#endif /* POWER_MANAGEMENT_H */
